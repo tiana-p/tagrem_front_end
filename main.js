341bbff2df24fe1e2282835c18718e3a6db41230
bananasplitter <tiana_pehle@yahoo.com>
2018-04-15 23:47:33 -0700

    Completed majority of front end work, still needs more done:

---
+++ main.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+    $("#cosemodal1").click(function(){
+        $("#productBox1").hide();
+    });
+    $("#learnmore1").click(function(){
+        $("#productBox1").show();
+    });
+});
+$(document).ready(function(){
+    $("#cosemodal2").click(function(){
+        $("#productBox2").hide();
+    });
+    $("#learnmore2").click(function(){
+        $("#productBox2").show();
+    });
+});
+$(document).ready(function(){
+    $("#cosemodal3").click(function(){
+        $("#productBox3").hide();
+    });
+    $("#learnmore3").click(function(){
+        $("#productBox3").show();
+    });
+});
+$(document).ready(function(){
+    $("#cosemodal4").click(function(){
+        $("#productBox4").hide();
+    });
+    $("#learnmore4").click(function(){
+        $("#productBox4").show();
+    });
+});
+
+
+
